7ff35528234430ef07dfce98966487deb9e4fa31
alert('     ') <70755554+alertIaN@users.noreply.github.com>
2022-11-15 19:36:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, Response
+import viberbot
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages import VideoMessage
+from viberbot.api.messages.text_message import TextMessage
+import logging
+
+from viberbot.api.viber_requests import ViberConversationStartedRequest
+from viberbot.api.viber_requests import ViberFailedRequest
+from viberbot.api.viber_requests import ViberMessageRequest
+from viberbot.api.viber_requests import ViberSubscribedRequest
+from viberbot.api.viber_requests import ViberUnsubscribedRequest
+
+app = Flask(__name__)
+viber = Api(BotConfiguration(
+    name='alert',
+    avatar='',
+    auth_token='4f23aa29a427e2fb-fa8e9a3060c3da70-6776e5a55140e12e'
+))
+
+
+@app.route('/', methods=['POST'])
+def incoming():
+    # every viber message is signed, you can verify the signature using this method
+    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+        return Response(status=403)
+
+    # this library supplies a simple way to receive a request object
+    viber_request = viber.parse_request(request.get_data())
+
+    if isinstance(viber_request, ViberMessageRequest):
+        message = viber_request.message
+        # lets echo back
+        viber.send_messages(viber_request.sender.id, [
+            message
+        ])
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [
+            TextMessage(text="thanks for subscribing!")
+        ])
+    elif isinstance(viber_request, ViberFailedRequest):
+        print('sosi')
+
+    return Response(status=200)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=4040, debug=True)
+    viber.set_webhook('https://953f-185-79-2-221.eu.ngrok.io')
